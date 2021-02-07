872f2ac9473cb331fd42b3abb2302db9f3784359
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-07 19:32:44 +0000

    red: excludes lines with both tax fields populated as they are exclusive

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -10,4 +10,14 @@ describe('CSV filter', () => {
 
 		expect(result).toEqual([header, invoiceLine]);
 	});
+
+	it('excludes lines with both tax fields populated as they are exclusive', () => {
+		const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+		const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
