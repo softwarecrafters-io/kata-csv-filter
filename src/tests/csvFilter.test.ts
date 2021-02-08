e1298ed35a45afa08c5bbb1ba566e3d167dc5a04
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-08 09:00:55 +0000

    refactor: replace constant with factory function

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -14,7 +14,7 @@ describe('CSV filter', () => {
 	});
 
 	it('excludes lines with both tax fields populated as they are exclusive', () => {
-		const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
+		const invoiceLine = fileWithOneInvoiceLineHaving('21', '7');
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
 		const result = csvFilter.filteredLines;
@@ -23,7 +23,7 @@ describe('CSV filter', () => {
 	});
 
 	it('excludes lines with both tax field empty as one is required', () => {
-		const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
+		const invoiceLine = fileWithOneInvoiceLineHaving('', '');
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
 		const result = csvFilter.filteredLines;
@@ -32,7 +32,7 @@ describe('CSV filter', () => {
 	});
 
 	it('excludes lines with non decimal tax fields', () => {
-		const invoiceLine = '1,02/05/2021,1000,790,XYZ,,ACER Laptop,B76430134,';
+		const invoiceLine = fileWithOneInvoiceLineHaving('XYZ', '');
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
 		const result = csvFilter.filteredLines;
@@ -41,7 +41,7 @@ describe('CSV filter', () => {
 	});
 
 	it('excludes lines with both tax fields populated even if non decimal', () => {
-		const invoiceLine = '1,02/05/2021,1000,790,XYZ,7,ACER Laptop,B76430134,';
+		const invoiceLine = fileWithOneInvoiceLineHaving('XYZ', '7');
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
 		const result = csvFilter.filteredLines;
