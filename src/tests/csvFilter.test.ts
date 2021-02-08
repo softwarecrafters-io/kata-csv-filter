cca0a81fbde90bb2b5b49301d34179fc2644b8fb
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-08 08:59:18 +0000

    refactor: extract factory function

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -2,9 +2,10 @@ import { CsvFilter } from '../core/csvFilter';
 
 describe('CSV filter', () => {
 	const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+	const emptyField = '';
 
 	it('allows for correct lines only', () => {
-		const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+		const invoiceLine = fileWithOneInvoiceLineHaving();
 		const csvFilter = CsvFilter.create([header, invoiceLine]);
 
 		const result = csvFilter.filteredLines;
@@ -47,4 +48,15 @@ describe('CSV filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	function fileWithOneInvoiceLineHaving(ivaTax = '21', igicTax = emptyField) {
+		const invoiceId = '1';
+		const invoiceDate = '02/05/2021';
+		const grossAmount = '1000';
+		const netAmount = '790';
+		const concept = 'ACER Laptop';
+		const cif = 'B76430134';
+		const nif = emptyField;
+		return [invoiceId, invoiceDate, grossAmount, netAmount, ivaTax, igicTax, concept, cif, nif].join(',');
+	}
 });
