7b8c268b1899c53ade19f51663428b8305c0a3aa
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-08 08:52:06 +0000

    green: excludes lines with both tax fields populated even if non decimal

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -13,8 +13,7 @@ export class CsvFilter {
 		const igicField = fields[5];
 		const decimalRegex = '\\d+(\\.\\d+)?';
 		const taxFieldsAreMutuallyExclusive =
-			(ivaField.match(decimalRegex) || igicField.match(decimalRegex)) &&
-			!(ivaField.match(decimalRegex) && igicField.match(decimalRegex));
+			(ivaField.match(decimalRegex) || igicField.match(decimalRegex)) && (!ivaField || !igicField);
 		if (taxFieldsAreMutuallyExclusive) {
 			result.push(this.lines[1]);
 		}
